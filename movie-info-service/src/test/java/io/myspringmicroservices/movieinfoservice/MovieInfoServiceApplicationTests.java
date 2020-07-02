4b0e760f74c6dcdf6a7bef8c2d7d6ffbe2333e07
James Joy <jamesjp296@gmail.com>
2020-07-02 17:59:49 +1200

    01. Init 3 projects

---
+++ movie-info-service/src/test/java/io/myspringmicroservices/movieinfoservice/MovieInfoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.myspringmicroservices.movieinfoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieInfoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
